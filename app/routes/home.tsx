c6ef410c41c1ed963ca0921e4b231002b36fa88f
Donna Cruz <130470174+donnac94@users.noreply.github.com>
2024-05-17 17:52:55 +0000

    update home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -12,11 +12,11 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 		// TODO: Verify signature against Noona public key
 		const idToken = jose.decodeJwt(url.searchParams.get('id_token')!);
 		const user = await getUserByCompanyId({ companyId: idToken['company_id'] as string });
-        const newAccessTokens = await refreshTokenExchange({ refreshToken: user.token.refreshToken });
-        console.log("User is ", user)
-        // console.log("company_id is ", idToken['company_id'] as string)
+		const newAccessTokens = await refreshTokenExchange({ refreshToken: user.token.refreshToken });
+		console.log('User is ', user);
+		// console.log("company_id is ", idToken['company_id'] as string);
 
-		return { user, accessToken: newAccessTokens.access_token};
+		return { user, accessToken: newAccessTokens.access_token };
 	} catch (exception) {
 		console.error(exception);
 	}
@@ -25,37 +25,53 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 };
 
 export default function Home() {
-    const { user, accessToken} = useLoaderData<typeof loader>();
-    const [products, setProducts] = useState<any>(null);
-    if (!user) return null;
+	const { user, accessToken } = useLoaderData<typeof loader>();
+	const [products, setProducts] = useState<any>(null);
+	if (!user) return null;
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks, react-hooks/exhaustive-deps
-    useEffect(() => {
-    
-        if(accessToken){
-            getProducts(accessToken)
-        }
+	// eslint-disable-next-line react-hooks/rules-of-hooks, react-hooks/exhaustive-deps
+	useEffect(() => {
+		if (accessToken) {
+			getProducts(accessToken);
+		}
+	}, [user, accessToken]);
 
-    }, [user, accessToken]);
+	async function getProducts(access_token: any) {
+		const productsResponse1 = await listGroupsAndProducts(undefined, { headers: { Authorization: `Bearer ${accessToken}` } });
+		console.log('productsResponse1 is ', productsResponse1);
+		setProducts(productsResponse1);
+		return productsResponse1;
+	}
 
-    async function getProducts(access_token: any) {
-        const productsResponse1 = await listGroupsAndProducts(undefined, {headers: {Authorization: `Bearer ${accessToken}`}});
-        console.log("productsResponse1 is ", productsResponse1)
-        setProducts(productsResponse1);
-        return productsResponse1;
-    }
-
-    return (
-        <div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black">
-            <h1 className="text-6xl text-white font-bold mb-8">Hi, {user.email} 👋</h1>
-            <h3 className="text-4xl text-white font-bold mb-8">Welcome to app-template-remix</h3>
-            <p className="text-xl text-white mb-4">You are logged in 🎉</p>
-            {products&&products.data[0].group_products.map((product: any) => (
-                <div key={product.title} className='flex-row'>
-                    <p className="text-4xl text-white font-bold mb-8">{product.title}</p>
-                    <p className="text-4xl text-white font-bold mb-8"> {product.stock_level}</p>
-                </div>
-            ))}
-        </div>
-    );
+	return (
+		<div className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+			<h1 className="text-6xl text-white font-bold mb-8">Hi, {user.email} 👋</h1>
+			<h3 className="text-4xl text-white font-bold mb-8">Welcome to app-template-remix</h3>
+			<p className="text-xl text-white mb-4">You are logged in 🎉</p>
+			{products && (
+				<table className="table-auto text-white">
+					<thead>
+						<tr>
+							<th className="px-4 py-2">Name</th>
+							<th className="px-4 py-2">Quantity</th>
+							<th className="px-4 py-2">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{products.data[0].group_products.map((product: any) => (
+							<tr key={product.title}>
+								<td className="border px-4 py-2">{product.title}</td>
+								<td className="border px-4 py-2">{product.stock_level}</td>
+								<td className="border px-4 py-2">
+									<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert(`Adding product: ${product.title}`)}>
+										Add Product
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			)}
+		</div>
+	);
 }
